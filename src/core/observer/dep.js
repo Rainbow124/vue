d2006c13dab90cfebb50e88e4210f2a1e56fff72
Rainbow <rainbow1071@163.com>
2022-03-11 00:32:08 +0800

    docs: delete

--- src/core/observer/dep.js
+++ src/core/observer/dep.js
@@ -5,36 +5,41 @@ import { remove } from '../util/index'
 import config from '../config'
 
 let uid = 0
+// dep 是个可观察对象，可以有多个指令订阅它
 
 /**
  * A dep is an observable that can have multiple
  * directives subscribing to it.
  */
 export default class Dep {
+  // 静态属性， watcher 对象
   static target: ?Watcher;
+  // dep 实例 id
   id: number;
+  // dep 实例对应的 watcher 对象/订阅者数组
   subs: Array<Watcher>;
 
-  constructor () {
+  constructor() {
     this.id = uid++
     this.subs = []
   }
-
-  addSub (sub: Watcher) {
+  // 添加新的订阅者 watcher 对象
+  addSub(sub: Watcher) {
     this.subs.push(sub)
   }
-
-  removeSub (sub: Watcher) {
+  // 移除订阅者
+  removeSub(sub: Watcher) {
     remove(this.subs, sub)
   }
-
-  depend () {
+  // 将观察者对象和 watcher 建立依赖
+  depend() {
     if (Dep.target) {
+      // 如果 target 存在，把 Dep 对象添加到 watcher 的依赖中
       Dep.target.addDep(this)
     }
   }
-
-  notify () {
+  // 发布通知
+  notify() {
     // stabilize the subscriber list first
     const subs = this.subs.slice()
     if (process.env.NODE_ENV !== 'production' && !config.async) {
@@ -43,24 +48,29 @@ export default class Dep {
       // order
       subs.sort((a, b) => a.id - b.id)
     }
+    // 调用每个订阅者的 update 方法实现更新
     for (let i = 0, l = subs.length; i < l; i++) {
       subs[i].update()
     }
   }
 }
-
+// Dep.target 用来存放目前正在使用的 watcher
+// 全局唯一，并且一次只能有一个 watcher 被使用
 // The current target watcher being evaluated.
 // This is globally unique because only one watcher
 // can be evaluated at a time.
 Dep.target = null
 const targetStack = []
-
-export function pushTarget (target: ?Watcher) {
+// 入栈并将当前的 watcher 赋值给 Dep.target
+// 父子组件嵌套的时候先把父组件对应的 watcher 入栈
+// 再去处理子组件的 watcher ，子组件的处理完毕之后，再把父组件对应的 watcher 出栈， 继续操作
+export function pushTarget(target: ?Watcher) {
   targetStack.push(target)
   Dep.target = target
 }
 
-export function popTarget () {
+export function popTarget() {
+  // 出栈操作
   targetStack.pop()
   Dep.target = targetStack[targetStack.length - 1]
 }
--- src/core/observer/watcher.js
+++ src/core/observer/watcher.js
@@ -43,7 +43,7 @@ export default class Watcher {
   getter: Function;
   value: any;
 
-  constructor (
+  constructor(
     vm: Component,
     expOrFn: string | Function,
     cb: Function,
@@ -80,6 +80,8 @@ export default class Watcher {
     if (typeof expOrFn === 'function') {
       this.getter = expOrFn
     } else {
+      // expOrFn 是字符串的时候，例如 watch：{ 'person.name': function... }
+      // parsePath('person.name') 返回一个函数获取 person.name 的值 
       this.getter = parsePath(expOrFn)
       if (!this.getter) {
         this.getter = noop
@@ -99,7 +101,7 @@ export default class Watcher {
   /**
    * Evaluate the getter, and re-collect dependencies.
    */
-  get () {
+  get() {
     pushTarget(this)
     let value
     const vm = this.vm
@@ -126,7 +128,7 @@ export default class Watcher {
   /**
    * Add a dependency to this directive.
    */
-  addDep (dep: Dep) {
+  addDep(dep: Dep) {
     const id = dep.id
     if (!this.newDepIds.has(id)) {
       this.newDepIds.add(id)
@@ -140,7 +142,7 @@ export default class Watcher {
   /**
    * Clean up for dependency collection.
    */
-  cleanupDeps () {
+  cleanupDeps() {
     let i = this.deps.length
     while (i--) {
       const dep = this.deps[i]
@@ -162,7 +164,7 @@ export default class Watcher {
    * Subscriber interface.
    * Will be called when a dependency changes.
    */
-  update () {
+  update() {
     /* istanbul ignore else */
     if (this.lazy) {
       this.dirty = true
@@ -177,7 +179,7 @@ export default class Watcher {
    * Scheduler job interface.
    * Will be called by the scheduler.
    */
-  run () {
+  run() {
     if (this.active) {
       const value = this.get()
       if (
@@ -205,7 +207,7 @@ export default class Watcher {
    * Evaluate the value of the watcher.
    * This only gets called for lazy watchers.
    */
-  evaluate () {
+  evaluate() {
     this.value = this.get()
     this.dirty = false
   }
@@ -213,7 +215,7 @@ export default class Watcher {
   /**
    * Depend on all deps collected by this watcher.
    */
-  depend () {
+  depend() {
     let i = this.deps.length
     while (i--) {
       this.deps[i].depend()
@@ -223,7 +225,7 @@ export default class Watcher {
   /**
    * Remove self from all dependencies' subscriber list.
    */
-  teardown () {
+  teardown() {
     if (this.active) {
       // remove self from vm's watcher list
       // this is a somewhat expensive operation so we skip it
